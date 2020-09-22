dd984b24f67d044ad089ec52ea1a01bbf8a5ae5f
Alwyn Lombaard <developer@lombaard.co.uk>
2020-09-22 19:53:02 +0100

    Add a Sor Op 60 no 22 recording

--- _site/javascripts/playlist.js
+++ _site/javascripts/playlist.js
@@ -38,7 +38,7 @@
 					{'name':'Andantino in A minor - Carulli','length':'01:24','file':'Carulli_Antantino_20150705_113534.mp3'},
 					{'name':'Opus 60 No 1 - Sor','length':'01:05','file':'Sor_Opus_60_no_1_20151122_131513.mp3'},
 					{'name':'Opus 60 No 22 - Sor','length':'02:31','file':'Sor_Opus_60_No_22_20151031_214715.mp3'},
-					{'name':'Opus 60 No 22 - Sor (again)','length':'02:18','file':'Sor Opus 60 no 22 Slur study_20200917.mp3'}
+					{'name':'Opus 60 No 22 - Sor (again)','length':'02:18','file':'Sor_Opus_60_no_22_Slur_study_20200917.mp3'}
 					
 				];
 		$scope.trackCount = $scope.tracks.length;
